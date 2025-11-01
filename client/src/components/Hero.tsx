45b77787aff0a28c4bc8f8ecce0104754568106f
PriyanshuYadav-aizen <yadavkumarpriyanshu830@gmail.com>
2025-11-02 04:14:08 +0530

    redeploy

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,11 +1,11 @@
 import Navbar from "./Navbar";
-import heroImg from "../assets/WhatsApp Image 2025-10-31 at 13.29.08_3221df55.jpg";
+import heroImg from "../assets/hero.avif";
 
 export default function Hero() {
   return (
     <>
       <section className="w-full h-[100vh] md:h-[90vh] lg:h-[88vh] flex items-center justify-center relative">
-        <div className="absolute top-15 left-0 right-20 z-50 flex justify-center pointer-events-none">
+        <div className="absolute top-4 left-0 right-20 z-50 flex justify-center pointer-events-none">
           <div className="pointer-events-auto w-full">
             <Navbar />
           </div>
